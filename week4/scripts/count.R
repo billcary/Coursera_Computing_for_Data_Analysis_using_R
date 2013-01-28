09acf5c877e2537e18e1ff723bca5955bed72282
Bill Cary <bill.cary@gmail.com>
2013-01-27 20:17:26 -0500

    Initial completion of script.

--- week4/scripts/count.R
+++ week4/scripts/count.R
@@ -11,15 +11,34 @@ homicides <- readLines("./data/homicides.txt")
 
 count <- function(cause = NULL) {
   ## Check that "cause" is non-NULL; else throw error
+  if (cause == NULL){
+    stop("The argument 'cause' must be non-NULL.")
+  }
   
   ## Check that specific "cause" is allowed; else throw error
-  
+  allowable.causes <- c("asphyxiation", "blunt force", "other", "shooting", "stabbing", "unknown")
+  if (tolower(cause) %!in% allowable.causes){
+    stop("The value specified for 'cause' is not valid.")
+  }
+
   ## Read "homicides.txt" data file
   homicides <- readLines("./data/homicides.txt")
   
   ## Extract causes of death
+  ## In homicides.txt, we want to extract the cause of death. The cause is listed in the file
+  ## in the following format: <dd>Cause: shooting</dd>
+  
+  cause.of.death.pre1 <- regexpr("<dd>[C|c]ause:(.*?)</dd>", homicides)
+  
+  cause.of.death.pre2 <- regmatches(homicides, cause.of.death.pre1)
+  
+  cause.of.death <- gsub("<dd>[C|c]ause: |</dd>", "", cause.of.death.pre2)
+  
+  ## Convert all cause.of.death entries to lower case to facilitate case insensitive matching
+  cause.of.death <- tolower(cause.of.death)
   
   ## Return integer containing count of homicides for that cause
+  sum(cause.of.death == tolower(cause))
   
 }
   
