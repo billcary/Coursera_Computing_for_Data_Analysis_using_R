be16ddf34569bd1aaf13e403548e0a3f222d6910
Bill Cary <bill.cary@gmail.com>
2013-01-27 23:24:34 -0500

    Misc updates to agecount

--- week4/scripts/agecount.R
+++ week4/scripts/agecount.R
@@ -9,11 +9,12 @@ agecount <- function(age = NULL) {
   
   ## Extract ages of victims; ignore records where no age is
   ## given
-  age.pre1 <- regexpr("[male,|female,] (.*?)years old</dd>", homicides)
+  age.pre1 <- regexpr("[male,|female,](.*?)years old</dd>", homicides)
   
   age.pre2 <- regmatches(homicides, age.pre1)
   
-  age <- gsub("[male,|female,] | years old</dd>", "", age.pre2)
+  ## age <- gsub("[male,|female,] | years old</dd>", "", age.pre2)
+  age <- gsub("[0-9][0-9] | years old</dd>", "", age.pre2)
   
   ## Return integer containing count of homicides for that age
   
