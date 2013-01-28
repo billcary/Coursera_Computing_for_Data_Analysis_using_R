57a44761243f5846862beb8ac332d8c3be4fc9dd
Bill Cary <bill.cary@gmail.com>
2013-01-27 22:56:07 -0500

    Updates to agecount.

--- week4/scripts/agecount.R
+++ week4/scripts/agecount.R
@@ -1,11 +1,19 @@
 agecount <- function(age = NULL) {
   ## Check that "age" is non-NULL; else throw error
+  if (is.null(age)){
+    stop("The argument 'age' must be non-NULL.")
+  }  
   
   ## Read "homicides.txt" data file
+  homicides <- readLines("../data/homicides.txt")
   
   ## Extract ages of victims; ignore records where no age is
-  
   ## given
+  age.pre1 <- regexpr("[male,|female,] (.*?)years old</dd>", homicides)
+  
+  age.pre2 <- regmatches(homicides, age.pre1)
+  
+  age <- gsub("[male,|female,] | years old</dd>", "", age.pre2)
   
   ## Return integer containing count of homicides for that age
   
