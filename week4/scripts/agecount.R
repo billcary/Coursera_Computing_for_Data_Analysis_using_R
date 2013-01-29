e5d15f4cc13979aa606457f3badab7430cdb7f6c
Bill Cary <bill.cary@gmail.com>
2013-01-28 22:57:32 -0500

    Commit prior to submission for evaluation.

--- week4/scripts/agecount.R
+++ week4/scripts/agecount.R
@@ -9,13 +9,20 @@ agecount <- function(age = NULL) {
   
   ## Extract ages of victims; ignore records where no age is
   ## given
-  age.pre1 <- regexpr("[male,|female,](.*?)years old</dd>", homicides)
+  age.pre1 <- regexpr("[0-9]+. years old</dd>", homicides)
+  ##age.pre1 <- regexpr(" (.*?)years old</dd>", homicides)
   
   age.pre2 <- regmatches(homicides, age.pre1)
   
   ## age <- gsub("[male,|female,] | years old</dd>", "", age.pre2)
-  age <- gsub("[0-9][0-9] | years old</dd>", "", age.pre2)
+  age.vector <- gsub(" years old</dd>", "", age.pre2)
+  
+  ## Convert age values from character to integer
+  age.vector <- as.numeric(age.vector)
   
   ## Return integer containing count of homicides for that age
+  count.age <- length(age.vector[age.vector==age])
+  
+  return(count.age)
   
 }
